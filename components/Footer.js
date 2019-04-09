8ce5757d6280d06d83d8226eb582aa13da16947d
Michael Aufreiter <michael@michael.local>
2019-04-09 10:03:57 +0200

    Footer WIP.

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,24 @@ export default function Footer() {
     return (
       <div className="sc-footer">
         <div className="se-footer-container">
-          Footer comes here.
+          <div className="se-col">
+            COL1
+          </div>
+          <div className="se-col">
+            <div class="nav-header">Products</div>
+            <div class="nav-item"><a href="/texture">Texture</a></div>
+            <div class="nav-item"><a href="/dar">DAR</a></div>
+            <div class="nav-item"><a href="/substance-js">Substance.io</a></div>
+          </div>
+          <div className="se-col">
+            <div class="nav-header">Products</div>
+            <div class="nav-item"><a href="/authoring">Scientific Authoring</a></div>
+            <div class="nav-item"><a href="/publishing">Journal Publishing</a></div>
+            <div class="nav-item"><a href="/repro-docs">Reproducible Documents</a></div>
+          </div>
+          <div className="se-col">
+            COL4
+          </div>
         </div>
       </div>
     )
