0ec8e6d710b8c6cc2f0bfa03935c69732a81ac50
Michael Aufreiter <michael@michael.local>
2019-05-18 23:56:25 +0200

    Deploy to gh-pages.

--- out/_next/static/chunks/styles.js
+++ out/_next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1558213316046");
+            injectCss(link, link.href.split("?")[0] + "?unix=1558216303745");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1558213632787");
+            injectCss(link, link.href.split("?")[0] + "?unix=1558216519502");
           });
       }
     }
